6960a538b8fa1f42a1cb0c66e99706ca5693130f
philogicae <38438271+philogicae@users.noreply.github.com>
2025-04-04 21:15:38 +0300

    contract 4: shuffled board stored on 18 bytes

--- stylus-contracts/src/lib.rs
+++ stylus-contracts/src/lib.rs
@@ -3,7 +3,7 @@ extern crate alloc;
 
 use alloy_primitives::{Address, FixedBytes, U256};
 use alloy_sol_types::sol;
-use stylus_sdk::{block, console, prelude::*};
+use stylus_sdk::{console, prelude::*};
 
 sol_storage! {
     #[entrypoint]
@@ -25,24 +25,31 @@ sol_storage! {
     pub struct Player { // by address
         string username;
         uint256 nb_games;
+        uint256 nb_wins;
         mapping(uint256 => uint256) game_ids;
     }
 }
 
 sol! {
-    error UsernameAlreadyExists(string username);
+    error UserNotInitialized();
+    error UserAlreadyExists(string username);
+    error UserNotFound(string username);
+    error InvalidOperation(string message);
 }
 
 #[derive(SolidityError)]
 pub enum GygesError {
-    UsernameAlreadyExists(UsernameAlreadyExists),
+    UserNotInitialized(UserNotInitialized),
+    UserAlreadyExists(UserAlreadyExists),
+    UserNotFound(UserNotFound),
+    InvalidOperation(InvalidOperation),
 }
 
 #[public]
 impl Gyges {
     pub fn register_username(&mut self, username: String) -> Result<(), GygesError> {
         if self.usernames.getter(username.clone()).get() != Address::new([0; 20]) {
-            return Err(GygesError::UsernameAlreadyExists(UsernameAlreadyExists {
+            return Err(GygesError::UserAlreadyExists(UserAlreadyExists {
                 username: username.clone(),
             }));
         }
@@ -52,7 +59,7 @@ impl Gyges {
             .setter(sender)
             .username
             .set_str(username.clone());
-        console!("Username '{}' registered", username);
+        console!("Username '{}' registered", username); // TODO: remove
         Ok(())
     }
 
@@ -69,6 +76,72 @@ impl Gyges {
         let address = self.get_address_by_username(username);
         let player = self.get_player_by_address(address);
         (address, player.0, player.1)
+    }
+
+    pub fn new_game(&mut self, adversary: String) -> Result<U256, GygesError> {
+        let sender = self.vm().msg_sender();
+        if self.players.get(sender).username.get_string() == "" {
+            return Err(GygesError::UserNotInitialized(UserNotInitialized {}));
+        }
+        let adversary_addr = self.get_address_by_username(adversary.clone());
+        if adversary_addr == Address::new([0; 20]) {
+            return Err(GygesError::UserNotFound(UserNotFound {
+                username: adversary,
+            }));
+        }
+        /* if adversary_addr == sender {
+            return Err(GygesError::InvalidOperation(InvalidOperation {
+                message: "Cannot play against yourself".to_string(),
+            }));
+        } */
+        // TODO: for quick tests
+        let game_id = self.nb_games.get();
+        let new_board = self.gen_new_board();
+        // TODO: finish setup
+        console!("Game {} created: {}", game_id, new_board);
+        Ok(game_id)
+    }
+}
+
+impl Gyges {
+    pub fn lcg_step(&self, seed: &mut u64) -> u64 {
+        let a: u64 = 1664525;
+        let c: u64 = 1013904223;
+        *seed = seed.wrapping_mul(a).wrapping_add(c);
+        *seed
+    }
+
+    pub fn shuffle<T>(&self, slice: &mut [T], seed: &mut u64) {
+        for i in (1..6).rev() {
+            let random_val = self.lcg_step(seed);
+            let j = (random_val % (i as u64 + 1)) as usize;
+            slice.swap(i, j);
+        }
+    }
+
+    pub fn gen_new_board(&self) -> FixedBytes<32> {
+        let mut timestamp = self.vm().block_timestamp();
+        let mut first_row: [u8; 6] = [1, 1, 2, 2, 3, 3];
+        self.shuffle(&mut first_row, &mut timestamp);
+        let mut last_row: [u8; 6] = [3, 3, 2, 2, 1, 1];
+        self.shuffle(&mut last_row, &mut timestamp);
+        let fst_row = [
+            (first_row[0] << 4) | first_row[1],
+            (first_row[2] << 4) | first_row[3],
+            (first_row[4] << 4) | first_row[5],
+        ]
+        .to_vec();
+        let lst_row = [
+            (last_row[0] << 4) | last_row[1],
+            (last_row[2] << 4) | last_row[3],
+            (last_row[4] << 4) | last_row[5],
+        ]
+        .to_vec();
+        FixedBytes::<32>::from_slice(
+            [fst_row, [0; 12].to_vec(), lst_row, [0; 14].to_vec()]
+                .concat()
+                .as_slice(),
+        )
     }
 }
 
