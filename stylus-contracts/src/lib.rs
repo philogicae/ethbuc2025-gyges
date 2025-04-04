1ae7382a9bcf9bbb3b343944fcd69f11356f4505
philogicae <38438271+philogicae@users.noreply.github.com>
2025-04-05 02:27:44 +0300

    contract 5: storage layout done

--- stylus-contracts/src/lib.rs
+++ stylus-contracts/src/lib.rs
@@ -3,7 +3,8 @@ extern crate alloc;
 
 use alloy_primitives::{Address, FixedBytes, U256};
 use alloy_sol_types::sol;
-use stylus_sdk::{console, prelude::*};
+use stylus_sdk::prelude::*;
+use stylus_sdk::stylus_core::log;
 
 sol_storage! {
     #[entrypoint]
@@ -15,11 +16,9 @@ sol_storage! {
     }
 
     pub struct Game { // by id
-        address playerone;
-        address playertwo;
-        bytes32 state; // board, turn, win
-        uint256 start;
-        uint256 end;
+        address player_one;
+        address player_two;
+        bytes32 state; // bytes: 6x6-board=18, padding=2, start=4, end=4, turn=4
     }
 
     pub struct Player { // by address
@@ -31,6 +30,8 @@ sol_storage! {
 }
 
 sol! {
+    event GameCreated(uint256 game_id, address player_one, address player_two);
+
     error UserNotInitialized();
     error UserAlreadyExists(string username);
     error UserNotFound(string username);
@@ -59,7 +60,6 @@ impl Gyges {
             .setter(sender)
             .username
             .set_str(username.clone());
-        console!("Username '{}' registered", username); // TODO: remove
         Ok(())
     }
 
@@ -78,7 +78,7 @@ impl Gyges {
         (address, player.0, player.1)
     }
 
-    pub fn new_game(&mut self, adversary: String) -> Result<U256, GygesError> {
+    pub fn new_game(&mut self, adversary: String) -> Result<(), GygesError> {
         let sender = self.vm().msg_sender();
         if self.players.get(sender).username.get_string() == "" {
             return Err(GygesError::UserNotInitialized(UserNotInitialized {}));
@@ -95,19 +95,59 @@ impl Gyges {
             }));
         } */
         // TODO: for quick tests
+
         let game_id = self.nb_games.get();
         let new_board = self.gen_new_board();
-        // TODO: finish setup
-        console!("Game {} created: {}", game_id, new_board);
-        Ok(game_id)
+
+        // Gyges layout
+        self.nb_games.set(game_id + U256::from(1));
+
+        // Game layout
+        let mut new_game = self.games.setter(game_id);
+        new_game.player_one.set(sender);
+        new_game.player_two.set(adversary_addr);
+        new_game.state.set(new_board);
+
+        // Player 1 layout
+        let player_one_nbg = self.players.get(sender).nb_games.get();
+        let mut player_one = self.players.setter(sender);
+        player_one.nb_games.set(player_one_nbg + U256::from(1));
+        player_one.game_ids.setter(player_one_nbg).set(game_id);
+
+        // Player 2 layout
+        let player_two_nbg = self.players.get(adversary_addr).nb_games.get();
+        let mut player_two = self.players.setter(adversary_addr);
+        player_two.nb_games.set(player_two_nbg + U256::from(1));
+        player_two.game_ids.setter(player_two_nbg).set(game_id);
+
+        log(
+            self.vm(),
+            GameCreated {
+                game_id,
+                player_one: sender,
+                player_two: adversary_addr,
+            },
+        );
+        Ok(())
+    }
+
+    pub fn game_by_id(&self, game_id: U256) -> (Address, Address, FixedBytes<32>) {
+        let game = self.games.get(game_id);
+        (
+            game.player_one.get(),
+            game.player_two.get(),
+            game.state.get(),
+        )
     }
 }
 
+// Private methods
 impl Gyges {
+    // Linear congruential generator for pseudo-random numbers
     pub fn lcg_step(&self, seed: &mut u64) -> u64 {
         let a: u64 = 1664525;
         let c: u64 = 1013904223;
-        *seed = seed.wrapping_mul(a).wrapping_add(c);
+        *seed = seed.wrapping_mul(a).wrapping_add(c); // ranqd1
         *seed
     }
 
@@ -120,32 +160,39 @@ impl Gyges {
     }
 
     pub fn gen_new_board(&self) -> FixedBytes<32> {
-        let mut timestamp = self.vm().block_timestamp();
+        let timestamp = self.vm().block_timestamp();
+        let mut seed = timestamp;
         let mut first_row: [u8; 6] = [1, 1, 2, 2, 3, 3];
-        self.shuffle(&mut first_row, &mut timestamp);
         let mut last_row: [u8; 6] = [3, 3, 2, 2, 1, 1];
-        self.shuffle(&mut last_row, &mut timestamp);
-        let fst_row = [
-            (first_row[0] << 4) | first_row[1],
-            (first_row[2] << 4) | first_row[3],
-            (first_row[4] << 4) | first_row[5],
-        ]
-        .to_vec();
-        let lst_row = [
-            (last_row[0] << 4) | last_row[1],
-            (last_row[2] << 4) | last_row[3],
-            (last_row[4] << 4) | last_row[5],
-        ]
-        .to_vec();
+
+        self.shuffle(&mut first_row, &mut seed);
+        self.shuffle(&mut last_row, &mut seed);
+
+        // Game state on a single bytes32
         FixedBytes::<32>::from_slice(
-            [fst_row, [0; 12].to_vec(), lst_row, [0; 14].to_vec()]
-                .concat()
-                .as_slice(),
+            [
+                vec![
+                    (first_row[0] << 4) | first_row[1],
+                    (first_row[2] << 4) | first_row[3],
+                    (first_row[4] << 4) | first_row[5],
+                ], // 1 row (3 bytes)
+                vec![0; 12], // 4 empty rows (12 bytes)
+                vec![
+                    (last_row[0] << 4) | last_row[1],
+                    (last_row[2] << 4) | last_row[3],
+                    (last_row[4] << 4) | last_row[5],
+                ], // 1 row (3 bytes)
+                vec![0; 2],  // padding (2 bytes)
+                timestamp.to_be_bytes()[4..].to_vec(), // start (4 bytes)
+                vec![0; 8],  // end=null (4 bytes) + turn=0 (4 bytes)
+            ]
+            .concat()
+            .as_slice(),
         )
     }
 }
 
-#[cfg(test)]
+/* #[cfg(test)]
 mod test {
     use super::*;
 
@@ -156,4 +203,4 @@ mod test {
         let mut _contract = Gyges::from(&vm);
         // assert_eq!(U256::ZERO, contract.number());
     }
-}
+} */
